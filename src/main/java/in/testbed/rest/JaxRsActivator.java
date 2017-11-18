0dcd83dcd181bd0259a8d6fbf00610ba9ddc47c7
abhiroop.nandiray@mindtree.com <abhiroop.nandiray@mindtree.com>
2017-11-18 21:07:45 +0530

    Initial commit

---
+++ src/main/java/in/testbed/rest/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package in.testbed.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/simplerest")
+public class JaxRsActivator extends Application {
+/* class body intentionally left blank */
+}
---
+++ src/main/java/in/testbed/rest/SimpleRest.java
@@ -0,0 +1,53 @@
+package in.testbed.rest;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/v1")
+public class SimpleRest {
+
+    @Path("/json")
+    public Response simpleJsonResponse(){
+        return Response.ok().entity(generateData()).build();
+    }
+
+
+    private Status generateData(){
+        Status status = new Status();
+        status.setCode(200);
+        status.setDesc("Simple desc");
+        status.setName("Simple name");
+        return status;
+    }
+
+    class Status{
+
+        private String name;
+        private String desc;
+        private int code;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+    }
+}
